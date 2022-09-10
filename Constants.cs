a6a148fc4724f03fee61fb997207efbd933e27b3
vahidzack <98702356+vahidzack@users.noreply.github.com>
2022-09-10 12:34:41 +0430

    test

--- Constants.cs
+++ Constants.cs
@@ -8,7 +8,7 @@ namespace MyWeatherApp
 {
     public static class Constants
     {
-
+        //xsaxsa
         public static string OpenWeathearMapEndpoint = "https://api.openweathermap.org/data/2.5/weather";
         public static string OpenWeathearMapApikey = "e458f26e0b1e0ed3164f38c75f93ce12"; 
      }
